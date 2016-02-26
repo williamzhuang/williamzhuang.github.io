6e6967d0f714ac245405400d63d1f5b737ef8bd6
William Zhuang <wzhuang1324@gmail.com>
2016-02-26 01:01:19 -0800

    Updated Game Design Page

--- js/script.js
+++ js/script.js
@@ -310,6 +310,26 @@ function rotateTo(angle) {
 		"-o-transform": "rotate("+nAngle+"deg) translateZ(1px)",
 		"transform": "rotate("+nAngle+"deg) translateZ(1px)",
 	});
+
+	/* Every time the window is scrolled ... */
+    $(window).scroll( function(){
+    
+        /* Check the location of each desired element */
+        $('.game').each( function(i){
+            
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height() + 200;
+            
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+                
+                $(this).animate({'opacity':'1'},500);
+                    
+            }
+            
+        }); 
+    
+    });
 }
 
 
